c62f3742e0cff0c46527f882d86dbf01ce43ce5b
it24104273 <it24104273@my.sliit.lk>
2024-11-24 21:43:44 +0530

    Update IT24104273Lab5Q2.java

--- Q2/IT24104273Lab5Q2.java
+++ Q2/IT24104273Lab5Q2.java
@@ -7,30 +7,46 @@ public class IT24104273Lab5Q2
 		Scanner input = new Scanner(System.in);
 		
 		System.out.print("Enter the number of new members introducted: ");
-		int num = input.nextInt();
+		int members = input.nextInt();
 		
 		System.out.print("\n");
 		
-		if (num == 0)
-		{System.out.print("No Prize");}
-	
-	    else if (num == 1)
-		{System.out.print("Prize is a : Pen");}	
-	
-	    else if (num == 2)
-		{System.out.print("Prize is a : Umbrella");}
-
-        else if (num == 3)
-        {System.out.print("Prize is a : Bag");}	
-	
-	    else if (num == 4)
-		{System.out.print("Prize is a : Travelling Chair");}	
-
-	    else if (num >= 5)
-		{System.out.print("Prize is a : Headphone");}
-	
-	    else if (num < 0)
+		if (members < 0)
 		{System.out.print("Input must be a number 0 or greater");}
+	
+	    else 
+		{
+		  switch (members)
+          
+		  {
+			case 0:
+			System.out.println("No prize");
+			break;
+			
+			case 1:
+			System.out.println("Prize is a : Pen");
+			break;
+			
+			case 2:
+			System.out.println("Prize is a : Umbrella");
+			break;
+			
+			case 3:
+			System.out.println("Prize is a : Bag");
+			break;
+			
+			case 4:
+			System.out.println("Prize is a : Travelling Chair");
+            break;
+
+            default:
+            System.out.println("Prize is a : Headphone");
+            break;
+            
+		    
+		  }		  
+		}
+		
 		
 		
 		
